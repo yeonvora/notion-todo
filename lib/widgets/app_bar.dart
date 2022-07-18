9832f6e9d44d99e6fdfe51898b8425aa5238fee9
reisyun <reisyun7@gmail.com>
2022-07-19 05:28:41 +0900

    fix: Change appbar ui design

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/styles/sizes.dart';
 import 'package:todolist/styles/colors.dart';
+import 'package:todolist/utils/get_today.dart';
 import 'package:todolist/widgets/common/text.dart';
 
 class TodoAppBar extends StatelessWidget {
@@ -18,31 +19,63 @@ class TodoAppBar extends StatelessWidget {
     return SliverAppBar(
       pinned: true,
       elevation: 0,
-      expandedHeight: 256,
+      toolbarHeight: 0,
+      expandedHeight: 240,
       backgroundColor: CommonColors.brand,
-      flexibleSpace: _buildFlexibleSpace(context),
-    );
-  }
-
-  Widget _buildFlexibleSpace(BuildContext context) {
-    // Divice status bar height
-    final double statusBarHeight = MediaQuery.of(context).padding.top;
-
-    return FlexibleSpaceBar(
-      centerTitle: true,
-      title: TodoText(
-        titles,
-        size: FontSizes.title,
-        color: FontColors.primary,
-        strong: true,
-      ),
-      collapseMode: CollapseMode.parallax,
-      background: Container(
-        padding: EdgeInsets.only(top: statusBarHeight),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [avatar ?? const SizedBox()],
+      flexibleSpace: FlexibleSpaceBar(
+        collapseMode: CollapseMode.pin,
+        background: Stack(
+          children: [
+            Container(
+              foregroundDecoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: FractionalOffset.bottomCenter,
+                    end: FractionalOffset.topCenter,
+                    colors: [
+                      CommonColors.background,
+                      Colors.transparent,
+                    ],
+                    stops: [
+                      0.1,
+                      0.8,
+                    ]),
+              ),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    'https://t1.daumcdn.net/cfile/tistory/9942CA415FCA28B40A',
+                  ),
+                  fit: BoxFit.cover,
+                  alignment: Alignment(0, -0.5),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12, bottom: 16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TodoText(
+                      getToday('yyyy년 M월 d일'),
+                      size: FontSizes.subHeader,
+                      color: FontColors.secondary,
+                      strong: true,
+                    ),
+                    const SizedBox(height: 4),
+                    TodoText(
+                      titles,
+                      size: FontSizes.headline,
+                      color: FontColors.primary,
+                      strong: true,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
