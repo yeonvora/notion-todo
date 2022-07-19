c9f36ceb0ef5e9fe62a745e92a9f99fbaa03131f
reisyun <reisyun7@gmail.com>
2022-07-19 21:07:19 +0900

    feat: Update notion controller

--- lib/api/notion_controller.dart
+++ lib/api/notion_controller.dart
@@ -1,34 +1,34 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:todolist/config/config.dart';
 import 'package:todolist/utils/get_today.dart';
 
-const NOTION_TOKEN = 'secret_5AxDZGYZc2c6j6CNeRYtwns1lXQRi7fSgtLxdgcIY4u';
-const NOTION_DATABASE_ID = 'a2a5fc5ecc78498da6a3742ff5c6ea1a';
-
 class NotionController {
-  Future post(dynamic data) async {
-    var uri = Uri.https('api.notion.com', '/v1/pages');
-    var header = {
-      'Authorization': 'Bearer $NOTION_TOKEN',
-      'Content-Type': 'application/json',
-      'Notion-Version': '2022-06-28',
-    };
+  static final header = {
+    'Authorization': 'Bearer $NOTION_TOKEN',
+    'Content-Type': 'application/json',
+    'Notion-Version': '2022-06-28',
+  };
+
+  Future createPage(List<dynamic> children) async {
+    final uri = Uri.https('api.notion.com', '/v1/pages');
 
     await http.post(
       uri,
       headers: header,
-      body: jsonEncode(data),
+      body: jsonEncode({
+        "parent": {"database_id": NOTION_DATABASE_ID},
+        "properties": {
+          "Date": {
+            "title": [
+              {
+                "text": {"content": "테스트 ### ${getToday('yyyy-MM-dd')}"}
+              }
+            ]
+          }
+        },
+        "children": children
+      }),
     );
   }
-}
-
-// Create a page
-dynamic createPage(List<dynamic> children) {
-  return {
-    'parent': {'database_id': NOTION_DATABASE_ID},
-    'properties': {
-      "Date": {"id": getToday()},
-    },
-    'children': children,
-  };
 }
---
+++ lib/config/config.exam.dart
@@ -0,0 +1,11 @@
+/// Where is Notion Token
+/// 1. 사이트 접속 - https://developers.notion.com
+/// 2. Notion 로그인
+/// 3. 새로운 API 생성
+/// 4. Secret Token 복사
+const NOTION_TOKEN = 'secret_oooooooooo';
+
+/// Where is Database Id
+/// 1. Notion Database 공유
+/// 2. 해시 값(database_id) 복사 - https://www.notion.so/database_id?v=...
+const NOTION_DATABASE_ID = 'oooooooooo';
