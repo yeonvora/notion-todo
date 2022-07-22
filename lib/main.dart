ab52bb6d85fd97820e1a3caa7a39afe767beeff6
reisyun <reisyun7@gmail.com>
2022-07-23 00:11:21 +0900

    feat: Add usecase for notion

--- lib/main.dart
+++ lib/main.dart
@@ -151,22 +151,32 @@ class _MainState extends State<Main> {
 
   /// 노션 동기화
   Future<void> synchronizeNotionPage() async {
-    final controller = NotionAPI();
+    final notion = NotionAPI();
     final usecase = sl.get<ActionUsecase>();
 
-    // 블록에 맞게 구체화
-    List<dynamic> actionBlockList(String type, List<Action> actions) => actions
+    final today = getToday('yyyy-MM-dd');
+    final pages = await notion.getPages(today);
+
+    // 페이지를 이미 생성한 경우 제거.
+    // why. 노션은 아직 블록 단위 업데이트를 지원 안함
+    // 따라서 Action을 추가한 경우 전체 페이지를 업데이트함
+    if (pages.isNotEmpty) {
+      await notion.removePage(pages[0]['id'] as String);
+    }
+
+    // 블록 형식에 맞게 구체화
+    List<dynamic> actionBlocks(String type, List<Action> actions) => actions
         .where((_) => _.type == type)
         .map((_) => checkboxBlock(_.name, _.done))
         .toList();
 
     // [1] 노션 페이지 생성
-    await controller.createPage(
-      "테스트 ### ${getToday('yyyy-MM-dd')}",
+    await notion.createPage(
+      today,
       [
-        ...actionBlockList(ActionType.routine, _actions),
+        ...actionBlocks(ActionType.routine, _actions),
         dividerBlock(),
-        ...actionBlockList(ActionType.task, _actions),
+        ...actionBlocks(ActionType.task, _actions),
       ],
     );
 
--- lib/notion/notion_api.dart
+++ lib/notion/notion_api.dart
@@ -2,9 +2,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:todolist/notion/notion_config.dart';
 
-// Notion API Reference
-// https://developers.notion.com/reference
+/// [Notion API Reference](https://developers.notion.com/reference)
 class NotionAPI {
+  static const domain = 'api.notion.com';
+
   static final header = {
     'Authorization': 'Bearer $NOTION_TOKEN',
     'Content-Type': 'application/json',
@@ -12,36 +13,56 @@ class NotionAPI {
   };
 
   Future<void> createPage(String title, List<dynamic> children) async {
-    final uri = Uri.https('api.notion.com', '/v1/pages');
+    final uri = Uri.https(domain, '/v1/pages');
 
-    await http
-        .post(
-          uri,
-          headers: header,
-          body: jsonEncode({
-            "parent": {"database_id": NOTION_DATABASE_ID},
-            "properties": {
-              "Date": {
-                "title": [
-                  {
-                    "text": {"content": title}
-                  }
-                ]
+    await http.post(
+      uri,
+      headers: header,
+      body: json.encode({
+        "parent": {"database_id": NOTION_DATABASE_ID},
+        "properties": {
+          "Date": {
+            "title": [
+              {
+                "text": {"content": title}
               }
-            },
-            "children": children
-          }),
-        )
-        .then((value) => print('fdas ${value.body}'));
+            ]
+          }
+        },
+        "children": children
+      }),
+    );
   }
 
   Future<void> removePage(String pageId) async {
-    final uri = Uri.https('api.notion.com', '/v1/pages/$pageId');
+    final uri = Uri.https(domain, '/v1/pages/$pageId');
 
     await http.patch(
       uri,
       headers: header,
-      body: jsonEncode({"archived": true}),
+      body: json.encode({"archived": true}),
     );
+  }
+
+  Future<List> getPages(String pageName) async {
+    final uri = Uri.https(domain, '/v1/databases/$NOTION_DATABASE_ID/query');
+
+    final response = await http.post(
+      uri,
+      headers: header,
+      body: json.encode({
+        "filter": {
+          "or": [
+            {
+              "property": "Date",
+              "title": {"equals": pageName}
+            }
+          ]
+        }
+      }),
+    );
+
+    // response: { object: 'database', results: [], ...}
+    return json.decode(response.body)['results'];
   }
 }
